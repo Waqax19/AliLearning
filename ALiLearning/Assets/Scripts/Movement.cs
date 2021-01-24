6554cd0ba6eacb2d1e54e98426c81e03916fadc4
Waqax19 <48880127+Waqax19@users.noreply.github.com>
2021-01-24 21:51:52 +0200

    Movement_And_Rotation

---
+++ ALiLearning/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   private Transform myTransform;
+   
+   public float maxSpeed = 5.0f;
+
+   //Awake function will be the first one , that will be activated 
+   private void Awake()
+   {
+        myTransform = GetComponent<Transform>();
+
+
+   }
+   
+    //called once per frame
+   private void Update()
+   {
+        myTransform.position += myTransform.forward * maxSpeed * Time.deltaTime;
+       
+   }
+
+
+}
---
+++ ALiLearning/Assets/Scripts/RotateTowards.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowards : MonoBehaviour
+{
+    private Transform myTransform;
+
+    public float rotationSpeed = 90f;
+
+    private void Awake()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+
+        myTransform.rotation *= Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, Vector3.up);
+    }
+}
