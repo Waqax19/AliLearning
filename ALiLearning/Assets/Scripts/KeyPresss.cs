af50ae342cf8e9c81e84d652d46a3897264e7643
Waqax19 <48880127+Waqax19@users.noreply.github.com>
2021-01-29 18:27:53 +0200

    RIghtMovement_HW_LEFT_UP_DOWN

---
+++ ALiLearning/Assets/Scripts/KeyPresss.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPresss : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Ali");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown("space"))
+      {
+          Debug.Log("Updating");
+      }
+
+      if(Input.GetKey("left"))
+      {
+          Debug.Log("Left Move");
+      }
+
+      if(Input.GetKey("right"))
+      {
+          transform.position += Vector3.right * speed * Time.deltaTime;
+      }
+    }
+}
---
+++ ALiLearning/Assets/Scripts/LookAndRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAndRotate : MonoBehaviour
+{
+    private Transform myObject = null;
+    public float rotationSpeed = 90.0f;
+    public Transform target = null;
+
+    private void Awake()
+    {
+        myObject = GetComponent<Transform>();
+    }
+
+
+    private void Update()
+    {
+
+        Quaternion destinationRotation = Quaternion.LookRotation(target.position - myObject.position, Vector3.up);
+
+        myObject.rotation = Quaternion.RotateTowards(myObject.rotation , destinationRotation, rotationSpeed * Time.deltaTime);
+        
+    }
+}
